ccc58f3e17e5b9dfdfb3677204ce335b5cd07788
martin-ax <arellanomartin123@gmail.com>
2022-05-24 19:42:31 -0700

    Added timeConversion

--- javascript hackerrank.js
+++ javascript hackerrank.js
@@ -34,10 +34,37 @@ function miniMaxSum(arr) {
     //print(minSum + ' ' + maxSum);
 }
 
+function timeConversion(s) {
+    // Write your code here
+    let result = '';
+    let pastNoon = s.substring(8,10) == 'AM'  ? false: true;
+
+    if (pastNoon) {
+        if (s.substring(0,2) === '12') {
+            result += s.substring(0,8);
+        } else {
+            result += String(Number(s.substring(0,2)) + 12) + s.substring(2,8);
+        }
+    } else {
+        if (s.substring(0,2) === '12') {
+            result += '00' + s.substring(2,8);
+        } else {
+            result += s.substring(0,8);
+        }
+    }
+    
+    console.log(s + ' is ' + result);
+}
+
 function main() {
 
     const arr = [1, 2, 3, 4, 5];
     miniMaxSum(arr);
+
+    let s = '07:05:45PM';
+    timeConversion(s);
+    timeConversion('12:01:00AM');
+    timeConversion('12:15:00PM');
 
 }
 
