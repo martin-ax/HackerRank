33627de00357df98691311803f1f994be9553919
martin-ax <arellanomartin123@gmail.com>
2022-05-31 11:56:43 -0700

    Added more solutions

--- javascript hackerrank.js
+++ javascript hackerrank.js
@@ -164,6 +164,64 @@ function countingSort(arr) {
     return result;
 }
 
+function pangrams(s) {
+    let map = new Map();
+    s = s.toLowerCase().replace(/ /g, '');
+    //console.log(s);
+    
+    if (s.length < 26) return 'not pangram';
+    
+    let letter = '';
+    for(let i = 0, length = s.length; i < length; i++) {
+        letter = s.charAt(i);
+        if(!map.has(letter)) {
+            map.set(letter, 1);
+        }
+    }
+    
+    //console.log(map.size);
+    if (map.size === 26) {
+        return 'pangram';
+    } else {
+        return 'not pangram';
+    }
+}
+
+function twoArrays(k, A, B) {
+    // Didn't get the full solution
+    // Easier to sort A biggest to smallest then B smallest to biggest
+    //   then have something like A[i] + B[i] >= k
+
+    /*
+    I saw someone sort smallest to biggest with below, not sure how this logically works tho
+    arrayOfIntegers.sort(function (first, second) {
+        return first - second;
+    });
+    or
+    arrayOfIntegers.sort((first, second) => {
+        return first - second;
+    });
+    */
+
+    // With above help
+    let size = A.length;
+    
+    A.sort((a, b) => {
+       return a - b; 
+    });
+    B.sort((a, b) => {
+        return b - a;
+    });
+    
+    for (let i = 0; i < size; i++) {
+        if (A[i] + B[i] < k) {
+            return 'NO';
+        }
+    }
+    
+    return 'YES';
+}
+
 function main() {
 
     const arr = [1, 2, 3, 4, 5];
@@ -190,6 +248,8 @@ function main() {
 
     const arr3 = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33];
     countingSort(arr3);
+
+    console.log(pangrams('We promptly judged antique ivory buckles for the next prize'));
 
 }
 
